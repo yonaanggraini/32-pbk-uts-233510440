b390b6a71a4cd2bd586f76acdefd565695ebd285
yonatrianggraini <yonatrianggraini56@gmail.com>
2025-05-06 18:15:01 +0700

    Struktur Dasar dan Tampilan Awal

---
+++ src/components/Todo-app.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const activities = ref([]);
+</script>
+
+<template>
+  <div class="page-wrapper">
+    <div class="container">
+      <h2 class="title">Daftar Kegiatan</h2>
+      <ul class="activity-list">
+        <li v-if="activities.length === 0" class="activity-item empty-item">
+          Tidak ada kegiatan untuk ditampilkan.
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
