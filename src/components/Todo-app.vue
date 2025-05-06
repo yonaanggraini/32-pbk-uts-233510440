105acf93172bef51f29eb1bd889625f040582b07
yonatrianggraini <yonatrianggraini56@gmail.com>
2025-05-06 18:51:31 +0700

    Menambah Fungsi memfilter dan cekbox

--- src/components/Todo-app.vue
+++ src/components/Todo-app.vue
@@ -18,7 +18,9 @@ const removeItem = (index) => {
   items.value.splice(index, 1);
 };
 
-
+const toggleAvailability = (index) => {
+  items.value[index].available = !items.value[index].available;
+};
 
 const startEditing = (index) => {
   editingIndex.value = index;
@@ -36,7 +38,11 @@ const cancelEdit = () => {
   editingIndex.value = null;
 };
 
-
+const filteredItems = () => {
+  return showOnlyAvailable.value
+    ? items.value.filter(item => item.available)
+    : items.value;
+};
 
 const totalItems = () => items.value.length;
 const availableItems = () =>
@@ -53,15 +59,22 @@ const availableItems = () =>
         <button @click="addItem" class="add-button">Tambah</button>
       </div>
 
-
+      <label class="filter-label">
+        <input type="checkbox" v-model="showOnlyAvailable" />
+        Tampilkan hanya yang tersedia
+      </label>
 
       <div class="summary">
         <p>Total Barang: {{ totalItems() }} | Tersedia: {{ availableItems() }}</p>
       </div>
 
       <ul class="activity-list">
+        <li v-if="filteredItems().length === 0" class="activity-item empty-item">
+          Tidak ada barang untuk ditampilkan.
+        </li>
 
         <li v-for="(item, index) in filteredItems()" :key="index" class="activity-item">
+          <input type="checkbox" :checked="item.available" @change="toggleAvailability(index)" />
 
           <template v-if="editingIndex === index">
             <input v-model="editedName" class="input-field small" />
